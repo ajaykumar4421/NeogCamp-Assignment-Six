06a51b9aa30e54f5aa6f186c48096e9694cd341c
ajaykumar4421 <ajay.kumar4421@gmail.com>
2021-08-23 09:03:43 +0530

    fix - index.html

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,17 @@
 var txtInput = document.querySelector("#textarea");
 var btntranslate = document.querySelector("#click-translate");
+var btnClear = document.querySelector("#clear-translate");
 var btnOutput = document.querySelector("#btn-output");
 
 
-btntranslate.addEventListener("click",function callEventHanadler(){
-   btnOutput.innerText = txtInput.value;
-   
+btntranslate.addEventListener("click",function getServerUrl(){
+    serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    var inputTranslate = serverUrl +"?"+"text="+txtInput.value;
+    fetch(inputTranslate)
+    .then(response => response.json())
+    .then( out => {btnOutput.innerText = out.contents.translated})
+});
+
+btnClear.addEventListener("click",function clearEventHandler(){
+    btnOutput.innerText = '';
 });
